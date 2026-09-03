77ff11c43438401cb1bb2639c6f308aa26544e60
Bharath Kumar05 <kumarbharath9942@gmail.com>
2026-09-03 16:29:10 +0530

    Make Bharath admin superuser

---
+++ jobportal/migrations/0009_make_bharath_superuser.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+def make_bharath_superuser(apps, schema_editor):
+    User = apps.get_model("auth", "User")
+
+    User.objects.filter(username="Bharath").update(
+        is_staff=True,
+        is_superuser=True,
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("jobportal", "0008_quizquestionhistory"),
+    ]
+
+    operations = [
+        migrations.RunPython(make_bharath_superuser),
+    ]
